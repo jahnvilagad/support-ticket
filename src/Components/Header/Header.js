a66716c744c8c5a98b525c23d1b79c893e0c52bf
viralsolanki <viralsolanki2@gmail.com>
2023-10-19 15:36:53 +0530

    create folder

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import classes from './Header.css';
+
+const Header = () => {
+  return (
+    <Header className={classes.header}>
+        <img src='./assets/img/the-website-factory-london.png' className='' alt='logo'/>
+    </Header>
+  )
+}
+
+export default Header
