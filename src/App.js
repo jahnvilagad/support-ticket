860ac243846a8920b278ab335c0386d2737c690c
jahnvi <jahnvi@thewebsitefactory.london>
2023-10-22 18:05:15 +0530

    create map fuc

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+// import { Profiler } from 'react';
 import './App.css';
 // import Header from './Components/Header/Header';
+import { recipes } from './Data';
 
-const Person = {
-  name: 'text'
-}
+// function Person()  {
+//     return (
+//       <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson"/>
+//     )
+// }
+
 
 function App() {
   return (
-    <div className="App">
-        <h1>{Person.name}</h1>
+    <div>
+      <h1>Recipes</h1>
+      {recipes.map(recipe =>
+        <div key={recipe.id}>
+          <h2>{recipe.name}</h2>
+          <ul>
+            {recipe.ingredients.map(ingredient =>
+              <li key={ingredient}>
+                {ingredient}
+              </li>
+            )}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
